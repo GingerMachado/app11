ab4fffaed6c8fa49389bc29e28757ab16056f151
GingerMachado <gmelanie.machador@ug.edu.ec>
2020-07-09 10:39:05 -0500

    Implementacion boton atras y recorrido de preguntas

--- app/src/main/java/com/example/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 //import android.support.v7.app.AppCompatActivity;
 
@@ -13,27 +14,104 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button btnSiguiente;
+    private Button btnAtras;
+    private TextView txtPregunta;
+
+    private Pregunta[] preguntas={
+
+            new Pregunta(R.string.pregunta1,true),
+            new Pregunta(R.string.pregunta2,true),
+            new Pregunta(R.string.pregunta3,true),
+            new Pregunta(R.string.pregunta4,true),
+            new Pregunta(R.string.pregunta5,false),
+    };
+
+    int actual = 0;
+    //int actual2 = preguntas.length ;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mTrueButton=(Button)findViewById(R.id.true_button);
+        mFalseButton=(Button)findViewById(R.id.false_button);
+        btnSiguiente= (Button)findViewById(R.id.btn_siguiente);
+        btnAtras=(Button)findViewById(R.id.btn_atras);
+        txtPregunta=(TextView) findViewById(R.id.txt_pregunta);
+
+        setText();
+
+
+
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText( MainActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT).show();
+                //Toast.makeText( MainActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT).show();
 
+                verificarRespuesta(true);
             }
 
         });
-            mFalseButton=(Button)findViewById(R.id.false_button);
-            mFalseButton.setOnClickListener(new View.OnClickListener() {
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(MainActivity.this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(getApplicationContext(),R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+                    verificarRespuesta(false);
+
                 }
-    });
+        });
+
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                actual = (actual + 1)% preguntas.length;
+                setText();
+
+
+            }
+        });
+
+
+        btnAtras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //actual = preguntas.length -1;
+                //actual2 = preguntas.length -1;
+
+                if (actual == 0) {
+                    actual = preguntas.length - 1;
+                } else {
+                    actual--;
+                }
+                setText();
+
+            }
+        });
+
+
+
+
+
         //public void setContentView(int layoutResID){}
 }
+    void setText( ) {
+        txtPregunta.setText(preguntas[actual].getTextoId());
+
+    }
+
+    void verificarRespuesta(boolean result){
+        boolean respuesta =   preguntas[actual].isRespuesta();
+        if(respuesta == result){
+            Toast.makeText(getApplicationContext(),R.string.correct_toast,Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(getApplicationContext(),R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
 }
 
